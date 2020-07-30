df9c01aef39b8cd5f8051fb7e398c135c851ef33
IceMupppet <42532126+junreyd@users.noreply.github.com>
2020-07-30 21:45:32 +0800

    done in experience

--- src/components/Experience.js
+++ src/components/Experience.js
@@ -20,16 +20,58 @@ class Experience extends Component {
                         {/* Description */}
                         <blockquote className="blockquote bq-warning mb-4">
                             <div className="row"> <i className="fas fa-briefcase fa-x mb-1 mr-3 ml-3" aria-hidden="true" />
-                            <h5 className="font-weight-bold mb-3">Senior developer</h5>
+                            <h5 className="font-weight-bold mb-3">SharePoint Developer</h5>
                             </div>
-                            <p className="font-weight-bold ml-1 dark-grey-text mb-2">July, 2013 - Present (4 years)</p>
-                            <p className="mb-0 ml-1 light-grey-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. </p>
+                            <span className="mb-0 ml-1 light-grey-text">House of IT - Mandaue, Cebu, Philippines</span>
+                            <p className="font-weight-bold ml-1 dark-grey-text mb-2"><strong>August 2018 - Present</strong></p>
+                            <p className="mb-0 ml-1 light-grey-text">•	Developed chat box using third party firebase, to handle real-time data read and write.</p>
+                            <p className="mb-0 ml-1 light-grey-text">•	Trained Total Quality Management (TQM) members in internal web functions, including steps on how to independently make minor updates or changes.</p>
+                            <p className="mb-0 ml-1 light-grey-text">•	Writing cross-browser compliant XHTML, CSS & JavaScript.</p>
+                            <p className="mb-0 ml-1 light-grey-text">•	Created various custom web parts for client needs.</p>
+                            <p className="mb-0 ml-1 light-grey-text">•	Investigated and implemented useful SharePoint capabilities out-of-the-box.</p>
+                            <p className="mb-0 ml-1 light-grey-text">•	Developed and maintained score card KPI over 300 supported users.</p>
+                            <p className="mb-0 ml-1 light-grey-text">•	Analyzed project in every retrospective meeting and planned the goals for the next sprint of the project using agile methodology.</p>
+                            <p className="mb-0 ml-1 light-grey-text">•	Maintain existing database-driven web interfaces for rapid file transfer and data communication using SQL Server 2012 and utilizing various jQuery plugins.</p>
+                            <p className="mb-0 ml-1 light-grey-text">•	Designed and developed RESTful style web services to manipulate dynamic datasets using AJAX calls.</p>
+                            <p className="mb-0 ml-1 light-grey-text">•	Integrate SharePoint into mobile friendly user interface, and cross-browser compatibility. </p>
+                        </blockquote>
+                        <blockquote className="blockquote bq-warning mb-4">
+                            <div className="row"> <i className="fas fa-briefcase fa-x mb-1 mr-3 ml-3" aria-hidden="true" />
+                            <h5 className="font-weight-bold mb-3">Web Developer and Graphic designer</h5>
+                            </div>
+                            <span className="mb-0 ml-1 light-grey-text">Part time freelance Web Developer - Home Based</span>
+                            <p className="font-weight-bold ml-1 dark-grey-text mb-2"><strong>July 2018 - Dec. 2018 (6 months)</strong></p>
+                            <p className="mb-0 ml-1 light-grey-text">•	Supported Ogtong Beach Resort with completing the last 20% of their website completion.</p>
+                            <p className="mb-0 ml-1 light-grey-text">•	Saved the time and budget over 90% by building the service website in 2 weeks with proper function.</p>
+                            <p className="mb-0 ml-1 light-grey-text">•	Participating in discussions with clients to clarify what they want.</p>
+                            <p className="mb-0 ml-1 light-grey-text">•	Creating logos, banners and buttons for websites.</p>
+                            <p className="mb-0 ml-1 light-grey-text">•	Worked on multiple projects simultaneously to a high standard. </p>
+                            <p className="mb-0 ml-1 light-grey-text">•	Collaborated with team members to developed multiple web applications.</p>
+                            <p className="mb-0 ml-1 light-grey-text">•	Identifying and correcting software deficiencies.</p>
+                            <p className="mb-0 ml-1 light-grey-text">•	Worked with connections/clients developing web applications that include e-commerce, booking, portfolio, blog and multilingual functionalities.</p>
+                            <p className="mb-0 ml-1 light-grey-text">•	Developed UI/UX strategies that include wireframes and mockups and for small and medium-sized enterprises.</p>
+                            <p className="mb-0 ml-1 light-grey-text">•	Performed maintenance and updates to existing client Websites.</p>
+                            <p className="mb-0 ml-1 light-grey-text">•	Working in the evening and weekends, to meet the clients time frame. </p>
+
+                        </blockquote>
+                        <blockquote className="blockquote bq-warning mb-4">
+                            <div className="row"> <i className="fas fa-briefcase fa-x mb-1 mr-3 ml-3" aria-hidden="true" />
+                            <h5 className="font-weight-bold mb-3">Web developer</h5>
+                            </div>
+                            <span className="mb-0 ml-1 light-grey-text">Attention Grabbing Media (AGM) - Clearwater, Florida, USA - Home Based</span>
+                            <p className="font-weight-bold ml-1 dark-grey-text mb-2"><strong>June 2018 - August 2018 (3 months)</strong></p>
+                            <p className="mb-0 ml-1 light-grey-text">•	Developing websites that have a consistent feel and look throughout all web properties.</p>
+                            <p className="mb-0 ml-1 light-grey-text">•	Providing technical support to end users.</p>
+                            <p className="mb-0 ml-1 light-grey-text">•	Developing and maintaining the front end functionality of websites.</p>
+                            <p className="mb-0 ml-1 light-grey-text">•	Devising SEO strategies based around specific keywords.</p>
+                            <p className="mb-0 ml-1 light-grey-text">•	Fixed bugs from existing websites and implemented enhancements that significantly improved web functionality and speed.</p>
+                            <p className="mb-0 ml-1 light-grey-text">•	Design and developed user-friendly website to support company' s marketing and promotional needs which includes Sales Force integration and cross-browser compatibility.</p>
                         </blockquote>
                         <blockquote className="blockquote bq-warning mt-1 mb-4">
                             <div className="row"> <i className="fas fa-briefcase fa-x mb-1 mr-3 ml-3" aria-hidden="true" />
                             <h5 className="font-weight-bold mb-3">Graphic Designer and Video Animator</h5>
                             </div>
-                            <span className="mb-0 ml-1 light-grey-text">Minglanilla, Cebu, Philippines</span>
+                            <span className="mb-0 ml-1 light-grey-text">AGuyiknow IT Business Solution - Minglanilla, Cebu, Philippines</span>
                             <p className="font-weight-bold ml-1 dark-grey-text mb-2"><strong>January 2018 - May 2018 (5 months)</strong></p>
                             <p className="mb-0 ml-1 light-grey-text">•	Designed artwork for initial client concepts to final production preparation for various screen-printed materials.</p>
                             <p className="mb-0 ml-1 light-grey-text">•	Collaborated and worked closely with riggers, modelers, textures, creative leads and animators to design and developed animations for various digital projects.</p>
